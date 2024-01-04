b2fec22897cf0132968490f67aa56846b9d2d114
NelsonBotero1518 <nelsonkennedy2004@gmail.com>
2024-01-04 12:03:55 -0500

    cambios similarProduct

--- src/components/ProductIdPage/ProductIdCard.jsx
+++ src/components/ProductIdPage/ProductIdCard.jsx
@@ -87,101 +87,103 @@ const ProductIdCard = ({ productId }) => {
 
   return (
     <div className="productid">
-      <section className="section__title--productid">
-        <h3 className="title--productid">
-          <span className="link--productid">
-            <Link to="/">Home</Link>
-          </span>
-          <span className="brand--productid">
-            <p className="pointseparator--productid"></p>
-            {productId?.title}
-          </span>
-        </h3>
-      </section>
-      <article className="article__productid">
-        <section className="section__images--productid">
-          <section className="main_image">
-            <i className="bx bx-chevrons-left" onClick={handlePrevImage}></i>
-            <img
-              className="img__product"
-              src={currentImage}
-              alt={productId?.title}
-            />
-            <i className="bx bx-chevrons-right" onClick={handleNextImage}></i>
-          </section>
-
-          <div className="carrusel__img--product">
-            <ul className="ul__img">
-              {productId?.images.map((img) => (
-                <li
-                  key={img.id}
-                  onClick={() => handleClick(img.id)}
-                  className="li__img"
-                >
-                  <img
-                    className={`images__product ${
-                      selectedImages[img.id] ? "selected" : ""
-                    }`}
-                    src={img.url}
-                    alt={productId?.title}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
+      <div className="productid">
+        <section className="section__title--productid">
+          <h3 className="title--productid">
+            <span className="link--productid">
+              <Link to="/">Home</Link>
+            </span>
+            <span className="brand--productid">
+              <p className="pointseparator--productid"></p>
+              {productId?.title}
+            </span>
+          </h3>
         </section>
-
-        <section className="info__productid">
-          <h3 className="brand__info--productid">{productId?.brand}</h3>
-          <h2 className="title__info--productid">{productId?.title}</h2>
-          <p className="description__info--productid">
-            {productId?.description}
-          </p>
-          <section className="section__butonsandprice">
-            <div className="div__price-info">
-              <h3 className="price__info--label">Price</h3>
-              <span className="price__info--value">$ {productId?.price}</span>
-            </div>
-            <section className="div__quantity">
-              <h3 className="price__info--label">Quantity</h3>
-              <div className="btn__quantity">
-                <button className="quantity__btn" onClick={handleMinus}>
-                  -
-                </button>
-                <div className="value__quamtity">{quantity}</div>
-                <button className="quantity__btn" onClick={handlePlus}>
-                  +
-                </button>
-              </div>
+        <article className="article__productid">
+          <section className="section__images--productid">
+            <section className="main_image">
+              <i className="bx bx-chevrons-left" onClick={handlePrevImage}></i>
+              <img
+                className="img__product"
+                src={currentImage}
+                alt={productId?.title}
+              />
+              <i className="bx bx-chevrons-right" onClick={handleNextImage}></i>
             </section>
+
+            <div className="carrusel__img--product">
+              <ul className="ul__img">
+                {productId?.images.map((img) => (
+                  <li
+                    key={img.id}
+                    onClick={() => handleClick(img.id)}
+                    className="li__img"
+                  >
+                    <img
+                      className={`images__product ${
+                        selectedImages[img.id] ? "selected" : ""
+                      }`}
+                      src={img.url}
+                      alt={productId?.title}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </div>
           </section>
-          <div className="div__btn--addtocart">
-            <button onClick={handleAddToCart} className="btn__addtocart">
-              Add to cart
-            </button>
-          </div>
+
+          <section className="info__productid">
+            <h3 className="brand__info--productid">{productId?.brand}</h3>
+            <h2 className="title__info--productid">{productId?.title}</h2>
+            <p className="description__info--productid">
+              {productId?.description}
+            </p>
+            <section className="section__butonsandprice">
+              <div className="div__price-info">
+                <h3 className="price__info--label">Price</h3>
+                <span className="price__info--value">$ {productId?.price}</span>
+              </div>
+              <section className="div__quantity">
+                <h3 className="price__info--label">Quantity</h3>
+                <div className="btn__quantity">
+                  <button className="quantity__btn" onClick={handleMinus}>
+                    -
+                  </button>
+                  <div className="value__quamtity">{quantity}</div>
+                  <button className="quantity__btn" onClick={handlePlus}>
+                    +
+                  </button>
+                </div>
+              </section>
+            </section>
+            <div className="div__btn--addtocart">
+              <button onClick={handleAddToCart} className="btn__addtocart">
+                Add to cart
+              </button>
+            </div>
+          </section>
+        </article>
+        <section className="section__similarProducts">
+          <SimilarProducts
+            categoryId={productId?.category.id}
+            idProduct={productId?.id}
+          />
         </section>
-      </article>
-      <section className="section__similarProducts">
-        <SimilarProducts
-          categoryId={productId?.category.id}
-          idProduct={productId?.id}
-        />
-      </section>
-      <section
-        className={`modal__addtocart ${
-          showSuccessModal && activeId === productId?.id ? "showmodal" : ""
-        }`}
-      >
-        {showSuccessModal && activeId === productId?.id && <ModalAddCart />}
-      </section>
-      <section
-        className={`modal__addtocart ${showErrorModal ? "showmodal" : ""}`}
-      >
-        {showErrorModal && activeId === productId?.id && showSuccessModal && (
-          <ModalErrorAddToCart />
-        )}
-      </section>
+        <section
+          className={`modal__addtocart ${
+            showSuccessModal && activeId === productId?.id ? "showmodal" : ""
+          }`}
+        >
+          {showSuccessModal && activeId === productId?.id && <ModalAddCart />}
+        </section>
+        <section
+          className={`modal__addtocart ${showErrorModal ? "showmodal" : ""}`}
+        >
+          {showErrorModal && activeId === productId?.id && showSuccessModal && (
+            <ModalErrorAddToCart />
+          )}
+        </section>
+      </div>
     </div>
   );
 };
